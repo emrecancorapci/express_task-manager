53f79795227d093a280916a3ccb0d165272bfbc9
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-02-14 20:31:58 +0300

    created backbone for tasks

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import Express from 'express';
+import taskRouter from './routes/tasks.js';
 
 const app = Express();
 const port = 3000;
 
+app.use(Express.json());
+
 app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
+app.use('/api/v1/tasks', taskRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
---
+++ src/controllers/tasks.js
@@ -0,0 +1,20 @@
+export const getAllTasks = (req, res) => {
+    return res.send('All items');
+};
+
+export const getTask = (req, res) => {
+    return res.send('Get item');
+};
+
+export const createTask = (req, res) => {
+    return res.send('Create item');
+};
+
+export const updateTask = (req, res) => {
+    return res.send('Update item');
+}
+
+export const deleteTask = (req, res) => {
+    return res.send('Delete item');
+}
+
