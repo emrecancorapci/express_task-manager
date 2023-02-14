f09d6160741bbde1ca1f69ff216f766af28b883b
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-02-14 22:15:45 +0300

    model added

---
+++ src/models/task.js
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+
+const TaskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export default model('Task', TaskSchema);
